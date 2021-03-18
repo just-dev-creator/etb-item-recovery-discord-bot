73db0283313cb86bbdef342a6fc50d9b17f359f3
just-dev-creator <justus@justus-alvermann.de>
2021-03-18 10:08:48 +0100

    Reformatted file using PyCharm

--- main.py
+++ main.py
@@ -12,26 +12,27 @@ client = commands.Bot(command_prefix='%')
 token = os.environ["DISCORD_BOT_TOKEN"]
 slash = SlashCommand(client, override_type=True)
 
+
 @client.event
 async def on_ready():
-  """Is called when the client logged in succesfully."""
-  # Notify user when client is connected
-  print('We have logged in as {0.user}'.format(client))
-  # Synchronise all commands because the cog is now loaded
-  await slash.sync_all_commands(delete_from_unused_guilds=False)
+    """Is called when the client logged in succesfully."""
+    # Notify user when client is connected
+    print('We have logged in as {0.user}'.format(client))
+    # Synchronise all commands because the cog is now loaded
+    await slash.sync_all_commands(delete_from_unused_guilds=False)
+
 
 # MongoDB Connection
 mongoClient = pymongo.MongoClient(os.environ["MONGODB_CONNECTION_STRING"])
 db = mongoClient["etb-item-recovery"]
 collection = db["cases"]
 
-
 # Load cogs
 client.load_extension("slash")
 client.load_extension("dmconversation")
 # Run the bot
 try:
-  client.run(token)
+    client.run(token)
 except KeyboardInterrupt:
-  print("Goodbye.")
-  exit(0)
+    print("Goodbye.")
+    exit(0)
